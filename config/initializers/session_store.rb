6b0073ecc272024f34933468d17e7a7b38c35b46
Aaron <parisi.aaron@gmail.com>
2021-02-24 00:59:18 +0000

    changed key in session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-MyApp::Application.config.session_store :cookie_store, key: '_my_app_session', secure: Rails.env.production?
+MyApp::Application.config.session_store :cookie_store, key: '_session_id', secure: Rails.env.production?
