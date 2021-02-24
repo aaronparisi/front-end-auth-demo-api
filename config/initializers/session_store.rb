7e18670720de69f49084baf3152c820c0b22c1dc
Aaron <parisi.aaron@gmail.com>
2021-02-24 02:56:05 +0000

    session store syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
-Rails.application.config.session_store :cookie_store, {
-  :key => '_session_id',
+Rails.application.config.session_store :cookie_store, :key => '_session_id',
   :domain => :all,
   :same_site => :none,
   :secure => :true,
   :tld_length => 2
-}
