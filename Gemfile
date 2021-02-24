5183e7ce049eb660cc84ddfdc01eeee1964a9306
Aaron <parisi.aaron@gmail.com>
2021-02-24 18:44:19 +0000

    messing w session store config to get same site none

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'pry-rails'
 gem 'jquery-rails'
 gem 'faker'
 gem 'rack-cors', :require => 'rack/cors'
+gem 'secure_headers'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store, :key => '_session_id',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 3
+  :secure => :true
+
+Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+# Rails.application.config.secure_cookies = :true
